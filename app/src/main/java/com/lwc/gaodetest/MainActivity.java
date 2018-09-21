69635182e891f34f0fc52b005d78c71930ed10c9
lwcsir <lwc897498287@163.com>
2018-09-21 20:09:39 +0800

    first commit

---
+++ app/src/main/java/com/lwc/gaodetest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lwc.gaodetest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.findViewById(R.id.tv_start_nva).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SingleRouteCalculateActivity.class));
+            }
+        });
+    }
+}
